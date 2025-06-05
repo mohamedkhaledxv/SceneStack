6edc4e41df0b3f4b155474e8089011fce6db3aa9
mohamed-khaled-xv <mohamedkhaledxv8@gmail.com>
2025-06-05 20:50:57 +0300

    the layouts has been implemented (root layout and tabs layout), also the home screen of the app (first tab)

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,71 +2,77 @@ import { Tabs } from "expo-router";
 import "react-native-reanimated";
 import { icons } from "./../../constants/icons";
 import TabBarIcon from "./../../components/TabBarIcon";
-import {SafeAreaView} from "react-native-safe-area-context";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { View } from "react-native";
 
 export default function TabLayout() {
   return (
-            <View className="flex-1 bg-transparent">
-
-
-    <Tabs
-      screenOptions={{
-        tabBarStyle: {
-          height: 60, // ⬅️ Increase height here
-          paddingBottom: 10, // ⬅️ Adjust padding inside the tab bar
-          paddingTop: 10,
-          marginVertical: 30, // ⬅️ Add vertical margin
-          marginHorizontal: 10, // ⬅️ Add horizontal margin
-          borderRadius: 20, // ⬅️ Add rounded corners
-          backgroundColor: "#2C2C2E", // ⬅️ Set background color
-          position: "absolute", // ⬅️ Ensure it stays at the bottom
-          bottom: 0, // ⬅️ Position at the bottom
-        },
-        tabBarItemStyle: {
-          flex: 1, // ⬅️ Ensure each tab takes equal space
-          alignItems: "center", // ⬅️ Center items horizontally
-          justifyContent: "center", // ⬅️ Center items vertically
-        },
-
-
-
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarLabel: () => null, // hide label (we handle it in the component)
-          headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <TabBarIcon focused={focused} icon={icons.home} title="Home" />
-          ),
+    <View className="flex-1 bg-transparent">
+      <Tabs
+        screenOptions={{
+          tabBarStyle: {
+            height: 60, // ⬅️ Increase height here
+            paddingBottom: 10, // ⬅️ Adjust padding inside the tab bar
+            paddingTop: 10,
+            marginHorizontal: 10, // ⬅️ Add horizontal margin
+            marginVertical: 10, // ⬅️ Add vertical margin
+            borderRadius: 20, // ⬅️ Add rounded corners
+            backgroundColor: "#2C2C2E", // ⬅️ Set background color
+            position: "absolute", // ⬅️ Ensure it stays at the bottom
+            bottom: 0, // ⬅️ Position at the bottom
+            borderTopWidth: 0,
+            elevation: 0, // For Android
+            shadowOpacity: 0, // For iOS
+          },
+          tabBarItemStyle: {
+            flex: 1, // ⬅️ Ensure each tab takes equal space
+            alignItems: "center", // ⬅️ Center items horizontally
+            justifyContent: "center", // ⬅️ Center items vertically
+          },
         }}
-      />
-      <Tabs.Screen
-        name="bookmark"
-        options={{
-          title: "Bookmark",
-          tabBarLabel: () => null,
-          headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <TabBarIcon focused={focused} icon={icons.save} title="Bookmark" />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: "Profile",
-          tabBarLabel: () => null,
-          headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <TabBarIcon focused={focused} icon={icons.person} title="Profile" />
-          ),
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Home",
+            tabBarLabel: () => null, // hide label (we handle it in the component)
+            headerShown: false,
+            tabBarIcon: ({ focused }) => (
+              <TabBarIcon focused={focused} icon={icons.home} title="Home" />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="bookmark"
+          options={{
+            title: "Bookmark",
+            tabBarLabel: () => null,
+            headerShown: false,
+            tabBarIcon: ({ focused }) => (
+              <TabBarIcon
+                focused={focused}
+                icon={icons.save}
+                title="Bookmark"
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            tabBarLabel: () => null,
+            headerShown: false,
+            tabBarIcon: ({ focused }) => (
+              <TabBarIcon
+                focused={focused}
+                icon={icons.person}
+                title="Profile"
+              />
+            ),
+          }}
+        />
+      </Tabs>
     </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,44 @@
-import 'react-native-reanimated';
-import { Stack } from 'expo-router';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import './global.css';
+import "react-native-reanimated";
+import { Stack, SplashScreen } from "expo-router";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import "./global.css";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "inter-variable": require("./../assets/fonts/Inter-VariableFont_opsz,wght.ttf"),
+    "nunito-variable": require("./../assets/fonts/Nunito-VariableFont_wght.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <SafeAreaProvider>
       <SafeAreaView className="flex-1 bg-[#1C1C1E]">
         <Stack
           screenOptions={{
             contentStyle: {
-              backgroundColor: 'transparent',
+              backgroundColor: "transparent",
             },
           }}
         >
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen
-            name="(tabs)"
-            options={{ headerShown: false }}
+            name="movie/[id]"
+            options={{
+              headerShown: false,
+            }}
           />
+          <Stack.Screen name="cast/[id]" options={{ headerShown: false }} />
         </Stack>
       </SafeAreaView>
     </SafeAreaProvider>
